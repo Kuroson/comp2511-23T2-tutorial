29581933688fa23b94241ae2f170886694d907ad
Alvin Cherk <a.cherk@unsw.edu.au>
2023-06-28 13:01:51 +1000

    Friday stuff

--- tute04/src/LSP/Ambulance.java
+++ tute04/src/LSP/Ambulance.java
@@ -3,18 +3,20 @@ package LSP;
 public class Ambulance extends Car {
     @Override
     public void drive(double speed) {
-        // Speed: 0 <= speed <= 45
+        // Speed: 0 <= speed <= 180
         // Range of speed is now smaller
-        if (!(0 <= speed && speed <= 45)) {
+        if (!(0 <= speed && speed <= 180)) {
             throw new RuntimeException("Speed out of range");
         }
         // Do stuff
     }
 
     public static void main(String[] args) {
-        Car c = new Car();
+        Car c = new Ambulance();
         c.drive(0); // oks
         c.drive(90); // ok
         // c.drive(180); // doesn't work with car as expected
+        String[] s = { "1" };
+        System.out.println(s instanceof String[]);
     }
 }
--- tute04/src/LSP/Car.java
+++ tute04/src/LSP/Car.java
@@ -1,11 +1,21 @@
 package LSP;
 
 public class Car {
+    /**
+     * @precondition 0 <= speed <= 90
+     * @param speed
+     */
     public void drive(double speed) {
-        // Speed: 0 <= speed <= 90
         if (!(0 <= speed && speed <= 90)) {
             throw new RuntimeException(String.format("Speed, %f, out of range", speed));
         }
         // Do stuff
+    }
+
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.drive(0); // oks
+        c.drive(90); // ok
+        // c.drive(180); // doesn't work with car as expected
     }
 }
